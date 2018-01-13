183a639024ad1f0de549dffae9fb316a8aa3bcd2
fsnAltria <1695869349@qq.com>
2018-01-13 22:15:46 +0800

    jmj

---
+++ siemensdaemon.h
@@ -0,0 +1,14 @@
+// Siemens Signal assignment of siemensdaemon
+#define siemensdaemon_SHARED_MEMORY "C:\\automation\\shm\\siemens.shm"
+#define siemensdaemon_MAILBOX "C:\\automation\\mbx\\siemens.mbx"
+
+#define siemensdaemon_CYCLE1_BASE 0
+#define siemensdaemon_CYCLE1_NUM_BYTES 8
+
+#define siemensdaemon_CYCLE2_BASE 8
+#define siemensdaemon_CYCLE2_NUM_BYTES 8
+
+#define siemensdaemon_CYCLE3_BASE 16
+#define siemensdaemon_CYCLE3_NUM_BYTES 32
+
+#define siemensdaemon_SHARED_MEMORY_SIZE 48
